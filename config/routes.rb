dff78d46ce4274c353d4a30888e8ff95b5d51173
user <fourleafclover688@gmail.com>
2019-05-12 23:04:28 +0900

    106ページまで進めた

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :fan_comments
+  resources :reviews
+  resources :authors
+  resources :users
   resources :books
   get 'hello/index'
   get 'hello/view'
