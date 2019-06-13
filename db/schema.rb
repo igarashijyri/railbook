d64bc3edbc0ea6079936c6fae125928384c32526
igarashijyri <fourleafclover688@gmail.com>
2019-06-13 10:04:18 +0900

    テスト環境のマイグレートしてなかったからテスト失敗してた

--- db/schema.rb
+++ db/schema.rb
@@ -69,7 +69,7 @@ ActiveRecord::Schema.define(version: 2019_05_15_014808) do
   create_table "reviews", force: :cascade do |t|
     t.integer "book_id"
     t.integer "user_id"
-    t.integer "status"
+    t.integer "status", default: 0, null: false
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
